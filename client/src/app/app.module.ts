ee5deefeedcffd34496d8154f34934dd066c6388
Ali Dogan <alidogan.just.for.commits@gmail.com>
2019-05-02 20:25:51 +0200

    Add quiz component and route

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { QuizComponent } from './quiz/quiz.component';
+
 import { SidebarComponent } from './main_layout/sidebar/sidebar.component';
 import { TopbarComponent } from './main_layout/topbar/topbar.component';
 import { FooterComponent } from './main_layout/footer/footer.component';
@@ -24,6 +26,7 @@ import { MessagesDropdownComponent } from './main_layout/topbar/messages/message
     TopbarComponent,
     FooterComponent,
     LogoutModalComponent,
+    QuizComponent,
     MessagesDropdownComponent
   ],
   imports: [
--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -2,12 +2,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { QuizComponent } from './quiz/quiz.component';
 
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'quiz', component: QuizComponent },
 
     { path: '**', redirectTo: '' }
 ];
