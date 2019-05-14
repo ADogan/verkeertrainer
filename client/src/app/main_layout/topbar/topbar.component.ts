cdba6bd52c04769c690a5507f571dc17883647e6
Ali Dogan <alidogan.just.for.commits@gmail.com>
2019-05-14 23:11:11 +0200

    Move button to start quiz to topbar

--- client/src/app/main_layout/topbar/topbar.component.ts
+++ client/src/app/main_layout/topbar/topbar.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Event, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.css']
 })
-export class TopbarComponent {
+export class TopbarComponent implements OnInit {
+  currentpath: any;
+
+  constructor(private router: Router ) {
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe( ( event: Event) => {
+      if (event instanceof NavigationEnd ) {
+        this.currentpath = event.url;
+      }
+    });
+  }
 }
