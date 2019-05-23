edccf02714be864e6c376389779d5eea2ee814e6
Ali Dogan <alidogan.just.for.commits@gmail.com>
2019-05-23 21:25:57 +0200

    Add some info to the dashboard.

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuizService } from '../services/quiz.service';
+import { IQuiz } from 'src/app/models/quiz.model';
 
 @Component({
     templateUrl: 'home.component.html' ,
     styleUrls: ['./home.component.css']
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+
+    quizzes: IQuiz[];
+  constructor(private quizService: QuizService) { }
+
+  ngOnInit() {
+    this.quizzes = this.quizService.getAllQuizzes();
+  }
+}
